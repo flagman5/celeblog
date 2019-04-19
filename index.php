96840786ed89f8d9f198dae1963a4c6670278dcd
flagman5 <flagman5@gmail.com>
2019-04-19 00:42:17 -0400

    init

---
+++ index.php
@@ -0,0 +1,81 @@
+<html>
+<head>
+<title>Latest celebrity news and gossip</title>
+<link rel="STYLESHEET" type="text/css" href="main.css" />	
+</head>
+
+<body>
+	<div class="page">
+	
+		<div class="tp_banner">
+		<img src="images/h1.png" width="100%" height="100%">
+		</div>
+				
+		<div class="tp_lft_ad">
+		<script type="text/javascript"><!--
+		google_ad_client = "pub-7628656525912627";
+		/* 160x600, created 7/18/10 */
+		google_ad_slot = "2097072323";
+		google_ad_width = 160;
+		google_ad_height = 600;
+		//-->
+		</script>
+		<script type="text/javascript"
+		src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+		</script>
+		</div>
+		
+		<div class="tp_ad">
+			<script type="text/javascript"><!--
+			google_ad_client = "pub-7628656525912627";
+			/* 728x15, created 7/18/10 */
+			google_ad_slot = "0478323324";
+			google_ad_width = 528;
+			google_ad_height = 15;
+			//-->
+			</script>
+			<script type="text/javascript"
+			src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+			</script>
+		
+			<div class="main">
+		
+				<div class="post">
+					<h3>Is Jessica Simpson Pregnant?</h3>
+					<img src="celeb/jsp.jpg">
+					<p>
+					From the looks of this picture, it does not seem like it. 
+					It seems like she only put up some extra weight. 
+					The dress is so tight that it is showing her little belly which I believe is not a baby but just pure lard.
+					<br/><br/>
+					Nothing wrong with a little cushion for the pushing. Besides, it is so difficult to stay on a weight target if you can eat gourmet food every meal,
+					have people shop your groceries and getting nothing but the finest types of foods from Morton Williams or Dean and Deluca. 
+					If I was in her position, I would definitely take advantage of all the food and enjoy life to the fullest. 
+					Therefore conclusion: shes not pregnant.
+					</p>
+				</div>
+				
+				<div class="post_ad">
+				<script type="text/javascript"><!--
+				google_ad_client = "pub-7628656525912627";
+				/* 200x90, created 7/18/10 */
+				google_ad_slot = "9480033064";
+				google_ad_width = 200;
+				google_ad_height = 90;
+				//-->
+				</script>
+				<script type="text/javascript"
+				src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+				</script>
+				</div>
+				
+	
+			</div>
+		</div>
+		
+		
+		
+	</div>
+</body>
+
+</html>
